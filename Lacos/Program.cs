1ec56ce1228425d355a9ed07b0b405aca4fd169e
Luan-art <luanlf2003@gmail.com>
2024-04-18 16:02:05 -0300

    Add project files.

---
+++ Lacos/Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroInicial = 0, numeroFinal = 0, aux = 0;
+Console.WriteLine("Escreva o numeroInicial:");
+numeroInicial = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o numeroFinal:" );
+numeroFinal = int.Parse(Console.ReadLine());
+aux = numeroInicial + 1; 
+while (aux < numeroFinal)
+{
+    if(aux % 2 == 0)
+    {
+        Console.WriteLine(aux + " Numero par");
+        aux++;
+    }
+    else 
+    { 
+        Console.WriteLine(aux + " Numero Impar");
+        aux++;
+    }
+}
+
